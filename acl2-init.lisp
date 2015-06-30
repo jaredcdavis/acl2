c9a2c40e61e598bbf7fd1dbc3f906694e349ef43
Jared Davis <jared@kookamara.com>
2015-06-29 20:42:53 -0500

    Edit readme and startup banner with warnings that this is experimental and it is my fault.

--- acl2-init.lisp
+++ acl2-init.lisp
@@ -701,10 +701,14 @@ implementations.")
 ; Normally:
   "
  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- + WARNING: This is NOT an ACL2 release; it is a development snapshot. +
- + The authors of ACL2 consider such distributions to be experimental; +
- + they may be incomplete, fragile, and unable to pass our own         +
- + regression tests.                                                   +
+ + WARNING:  This is Jared's attempt to refactor ACL2's code into      +
+ + something he understands.  It is NOT an ACL2 release or, for that   +
+ + matter, even a development version of ACL2.  It is NOT sanctioned   +
+ + in any way by the authors of ACL2 and has not been reviewed by the  +
+ + authors of ACL2.  Jared may have introduced bugs, even soundness    +
+ + bugs, and is solely responsible for any such errors.  In short:     +
+ + this is experimental software.  Do not rely on this program for     +
+ + anything that matters for anything!                                 +
  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 "
   )
